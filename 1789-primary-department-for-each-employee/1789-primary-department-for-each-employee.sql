b4983b4c2ee9e47a34529a79955ba858fc78f642
baroel <73051141+akbarulazis@users.noreply.github.com>
2024-02-06 15:28:47 +0700

    Time: 832 ms (99.27%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -2,7 +2,7 @@
 
 select distinct employee_id, department_id from (
 select * from employee a
-union all
+union
 select employee_id, department_id, 'Y' primary_flag from employee
           group by employee_id having Count(*) = 1
 ) k where primary_flag = 'Y'
