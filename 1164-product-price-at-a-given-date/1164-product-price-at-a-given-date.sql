f343a0b921fd2996a045ea99b36e9dd4a389eede
baroel <73051141+akbarulazis@users.noreply.github.com>
2024-02-07 09:25:53 +0700

    Time: 718 ms (93.88%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,17 +1,17 @@
-# Write your MySQL query statement below
 
-select a.product_id, coalesce(new_price,10) price
-from (
-select distinct product_id from products) a
-left join (
-select a.product_id, new_price
-from products a
-left join (
-select product_id, max(change_date) mx_date from products
-    where change_date <= '2019-08-16'
+
+select distinct product_id, 10 price from products
+where product_id not in (select product_id from products where 
+                        change_date <= '2019-08-16'
+                        )
+union all
+select distinct product_id, new_price price from products
+where (product_id,change_date) in (
+select product_id, max(change_date) from products where
+                      change_date <= '2019-08-16'
     group by 1
-) b
-on a.product_id =  b.product_id and a.change_date = b.mx_date
-where mx_date is not null 
-) b
-on a.product_id =  b.product_id
+                      
+)
+
+;
+
