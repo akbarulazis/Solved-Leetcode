00339ea374928ec893dba3a295b1816d1396ce12
baroel <73051141+akbarulazis@users.noreply.github.com>
2025-01-17 13:14:55 +0700

    2789-COUNTER-II Stats: Time: 60 ms (51.18%), Space: 52.3 MB (8.92%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const originalvalue = init
+    return {
+
+    increment:function(){
+        init++
+        return init
+    },
+
+    decrement:function(){
+        init--
+        return init
+    },
+
+    reset:function(){
+        init = originalvalue
+        return init
+
+    }
+
+    
+
+
+
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
